88150c33d895c49b6a10972826d53c2671461bda
nilsnolde <nils.nolde@gmail.com>
2020-08-28 11:45:40 +0200

    then try docker service name as host.. damn it

--- test_pg.py
+++ test_pg.py
@@ -5,11 +5,13 @@ import psycopg2
 import sys, os
 
 p = dict(
-    host='localhost',
-    database=os.getenv('POSTGRES_DB'),
-    user=os.getenv('POSTGRES_USER'),
-    password=os.getenv('POSTGRES_PASS')
+    host=os.getenv('POSTGRES_PASS') or 'localhost',
+    port=os.getenv('POSTGRES_PASS') or 5432,
+    database=os.getenv('POSTGRES_DB') or 'gis_test',
+    user=os.getenv('POSTGRES_USER') or 'admin',
+    password=os.getenv('POSTGRES_PASS') or 'admin',
 )
+print(p)
 con = psycopg2.connect(**p)
 
 cur = con.cursor()
