cfa0cb4318bba45c2fa93f81bcc35c9350698c3e
nilsnolde <nils.nolde@gmail.com>
2020-08-28 12:02:41 +0200

    next try

--- test_pg.py
+++ test_pg.py
@@ -5,8 +5,8 @@ import psycopg2
 import sys, os
 
 p = dict(
-    host=os.getenv('POSTGRES_PASS') or 'localhost',
-    port=os.getenv('POSTGRES_PASS') or 5432,
+    host=os.getenv('POSTGRES_HOST') or 'localhost',
+    port=os.getenv('POSTGRES_PORT') or 5432,
     database=os.getenv('POSTGRES_DB') or 'gis_test',
     user=os.getenv('POSTGRES_USER') or 'admin',
     password=os.getenv('POSTGRES_PASS') or 'admin',
