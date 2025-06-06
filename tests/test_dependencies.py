90a391a1c1fe28565ef9de201ca61ba3bd5dea62
Christian Beiwinkel <chrstn.bwnkl@gmail.com>
2025-06-06 16:25:04 +0200

    fix test

--- tests/test_dependencies.py
+++ tests/test_dependencies.py
@@ -5,10 +5,10 @@ import pytest
 from fastapi import HTTPException
 from starlette.status import HTTP_400_BAD_REQUEST
 
-from routing_packager_app.api_v1.dependencies import split_bbox, get_validated_name, validate_bbox
+from routing_packager_app.api_v1.dependencies import split_bbox, get_validated_name
 
 
-@pytest.mark.parametrize("bbox", ("0;0;1;1", "0,2,3"))
+@pytest.mark.parametrize("bbox", ("0;0;1;1", "0,2,3", "20,20,20,20", "20,30,40,30", "120,50,110,60"))
 def test_split_bbox_fail(bbox):
     with pytest.raises((ValueError, HTTPException)):
         split_bbox(bbox)
@@ -18,11 +18,4 @@ def test_split_bbox_fail(bbox):
 def test_validate_names_fail(name):
     with pytest.raises(HTTPException) as e:
         get_validated_name(name)
-    assert e.value.status_code == HTTP_400_BAD_REQUEST
-
-
-@pytest.mark.parametrize("bbox", ("20,20,20,20", "20,30,40,30", "120,50,110,60"))
-def test_validate_bbox_fail(bbox):
-    with pytest.raises(HTTPException) as e:
-        validate_bbox(bbox)
     assert e.value.status_code == HTTP_400_BAD_REQUEST
